0137c82584b4332a594c502371742da1aac21bdb
David Szotten <davidszotten@gmail.com>
2019-01-09 21:29:42 +0000

    encryption works

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,41 @@
 use actix;
 use actix_web;
-// use env_logger;
 use futures;
 
 use actix_web::{
     client, server, App, AsyncResponder, Body, Error, HttpMessage,
     HttpResponse, http, Path, HttpRequest, FutureResponse
 };
+use clap;
 use futures::{Future, Stream};
 use md5::{Md5, Digest};
 
 /// streaming client request to a streaming server response
-fn streaming((path, req): (Path<String>, HttpRequest)) -> FutureResponse<HttpResponse> {
-// fn streaming(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
-// fn streaming(req: &HttpRequest) -> FutureResponse<HttpResponse> {
+fn streaming((path, req): (Path<String>, HttpRequest<State>)) -> FutureResponse<HttpResponse> {
     let query = req.query_string();
-    println!("{:?}", query);
-    // let email = query.get("email".into());
-    // let hash = if let Some(email) = email {
-    //     // println!("{:?}", email);
-    //     Md5::new().chain(email).result()
-    //     // let mut hasher = Md5::new();
-    //     // hasher.input(email);
-    //     // hasher.result()
-    // } else {
-    //     return Box::new(future::result::<HttpResponse, Error>(Ok(HttpResponse::build(http::StatusCode::NOT_FOUND).finish())))
-    // };
+    let password = &req.state().password;
 
-    println!("path: {}", path);
-    let hash = Md5::new().chain(path.as_ref()).result();
+    let fernet = fernet::Fernet::new(&password).unwrap();
+    let email = fernet.decrypt(&path.as_ref());
+    let email = email.unwrap();
+
+    let hash = Md5::new().chain(email).result();
     // send client request
     client::ClientRequest::get(format!("https://www.gravatar.com/avatar/{:x}?{}", hash, query))
         .finish().unwrap()
         .send()                         // <- connect to host and send request
         .map_err(Error::from)           // <- convert SendRequestError to an Error
         .and_then(|resp| {              // <- we received client response
-            Ok(HttpResponse::Ok()
+            let mut new_response = HttpResponse::Ok();
+            for (key, value) in resp.headers().into_iter() {
+                if key == http::header::CACHE_CONTROL ||
+                    key == http::header::DATE ||
+                    key == http::header::EXPIRES
+                {
+                    new_response.header(key.clone(), value.clone());
+                }
+            }
+            Ok(new_response
                // read one chunk from client response and send this chunk to a server response
                // .from_err() converts PayloadError to an Error
                .body(Body::Streaming(Box::new(resp.payload().from_err()))))
@@ -43,21 +43,39 @@ fn streaming((path, req): (Path<String>, HttpRequest)) -> FutureResponse<HttpRes
         .responder()
 }
 
+struct State {
+    password: String,
+}
+
 fn main() {
-    // ::std::env::set_var("RUST_LOG", "actix_web=info");
-    // env_logger::init();
     let sys = actix::System::new("http-proxy");
 
-    server::new(|| {
-        App::new()
-            // .middleware(middleware::Logger::default())
-            // .resource("/", |r| r.f(streaming))
-            .resource("/{path}", |r| r.method(http::Method::GET).with(streaming))
+    let matches =
+        clap::App::new("gravatar-proxy")
+            .about("Gravatar proxy")
+            .version("0.1.0")
+            .arg(clap::Arg::with_name("bind")
+                .help("Bind to a specific address (ip:port)")
+                .long("bind")
+                .value_name("ADDR")
+                .default_value("localhost:6000"))
+            .arg(clap::Arg::with_name("password")
+                .help("Password for encrypting the email addresses")
+                .long("password")
+                .value_name("PASSWORD")
+                .default_value("password"))
+            .get_matches();
+
+    let bind = matches.value_of("bind").unwrap();
+    let password = matches.value_of("password").unwrap().to_string();
+
+    server::new(move || {
+        App::with_state(State{password: password.clone()})
+            .resource("/avatar/{path}", |r| r.method(http::Method::GET).with(streaming))
     }).workers(1)
-        .bind("127.0.0.1:8080")
+        .bind(bind)
         .unwrap()
         .start();
 
-    // println!("Started http server: 127.0.0.1:8080");
     let _ = sys.run();
 }
