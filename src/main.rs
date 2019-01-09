563bf05cd208df639a8673ccb59e77e71f3919db
David Szotten <davidszotten@gmail.com>
2019-01-09 21:31:32 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use actix_web;
 use futures;
 
 use actix_web::{
-    client, server, App, AsyncResponder, Body, Error, HttpMessage,
-    HttpResponse, http, Path, HttpRequest, FutureResponse
+    client, http, server, App, AsyncResponder, Body, Error, FutureResponse, HttpMessage,
+    HttpRequest, HttpResponse, Path,
 };
 use clap;
 use futures::{Future, Stream};
-use md5::{Md5, Digest};
+use md5::{Digest, Md5};
 
 /// streaming client request to a streaming server response
 fn streaming((path, req): (Path<String>, HttpRequest<State>)) -> FutureResponse<HttpResponse> {
@@ -21,26 +21,31 @@ fn streaming((path, req): (Path<String>, HttpRequest<State>)) -> FutureResponse<
 
     let hash = Md5::new().chain(email).result();
     // send client request
-    client::ClientRequest::get(format!("https://www.gravatar.com/avatar/{:x}?{}", hash, query))
-        .finish().unwrap()
-        .send()                         // <- connect to host and send request
-        .map_err(Error::from)           // <- convert SendRequestError to an Error
-        .and_then(|resp| {              // <- we received client response
-            let mut new_response = HttpResponse::Ok();
-            for (key, value) in resp.headers().into_iter() {
-                if key == http::header::CACHE_CONTROL ||
-                    key == http::header::DATE ||
-                    key == http::header::EXPIRES
-                {
-                    new_response.header(key.clone(), value.clone());
-                }
+    client::ClientRequest::get(format!(
+        "https://www.gravatar.com/avatar/{:x}?{}",
+        hash, query
+    ))
+    .finish()
+    .unwrap()
+    .send() // <- connect to host and send request
+    .map_err(Error::from) // <- convert SendRequestError to an Error
+    .and_then(|resp| {
+        // <- we received client response
+        let mut new_response = HttpResponse::Ok();
+        for (key, value) in resp.headers().into_iter() {
+            if key == http::header::CACHE_CONTROL
+                || key == http::header::DATE
+                || key == http::header::EXPIRES
+            {
+                new_response.header(key.clone(), value.clone());
             }
-            Ok(new_response
-               // read one chunk from client response and send this chunk to a server response
-               // .from_err() converts PayloadError to an Error
-               .body(Body::Streaming(Box::new(resp.payload().from_err()))))
-        })
-        .responder()
+        }
+        Ok(new_response
+            // read one chunk from client response and send this chunk to a server response
+            // .from_err() converts PayloadError to an Error
+            .body(Body::Streaming(Box::new(resp.payload().from_err()))))
+    })
+    .responder()
 }
 
 struct State {
@@ -50,32 +55,40 @@ struct State {
 fn main() {
     let sys = actix::System::new("http-proxy");
 
-    let matches =
-        clap::App::new("gravatar-proxy")
-            .about("Gravatar proxy")
-            .version("0.1.0")
-            .arg(clap::Arg::with_name("bind")
+    let matches = clap::App::new("gravatar-proxy")
+        .about("Gravatar proxy")
+        .version("0.1.0")
+        .arg(
+            clap::Arg::with_name("bind")
                 .help("Bind to a specific address (ip:port)")
                 .long("bind")
                 .value_name("ADDR")
-                .default_value("localhost:6000"))
-            .arg(clap::Arg::with_name("password")
+                .default_value("localhost:6000"),
+        )
+        .arg(
+            clap::Arg::with_name("password")
                 .help("Password for encrypting the email addresses")
                 .long("password")
                 .value_name("PASSWORD")
-                .default_value("password"))
-            .get_matches();
+                .default_value("password"),
+        )
+        .get_matches();
 
     let bind = matches.value_of("bind").unwrap();
     let password = matches.value_of("password").unwrap().to_string();
 
     server::new(move || {
-        App::with_state(State{password: password.clone()})
-            .resource("/avatar/{path}", |r| r.method(http::Method::GET).with(streaming))
-    }).workers(1)
-        .bind(bind)
-        .unwrap()
-        .start();
+        App::with_state(State {
+            password: password.clone(),
+        })
+        .resource("/avatar/{path}", |r| {
+            r.method(http::Method::GET).with(streaming)
+        })
+    })
+    .workers(1)
+    .bind(bind)
+    .unwrap()
+    .start();
 
     let _ = sys.run();
 }
